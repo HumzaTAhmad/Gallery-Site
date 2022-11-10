6f247b6176ef2f2e46c8c67496ed116f2009aafe
HumzaTAhmad <humza3173@gmail.com>
2022-11-10 14:12:48 -0500

    Connect backend to mongodb cloud database
    Created environement variable to connec to mongodb atlas, as well as
    specifying the port it is intended to run on

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const app = express();
 
@@ -9,3 +12,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
+const PORT = process.env.PORT || 5000;
+
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message))
