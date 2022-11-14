9ad93767bc82074e3397e2469e7e00548388e5da
HumzaTAhmad <humza3173@gmail.com>
2022-11-13 22:35:35 -0500

    Create updated and delete rest endpoint for site
    Created router functionality so picture can be deleted and updated in the
    database.

--- server/controllers/pictures.js
+++ server/controllers/pictures.js
@@ -14,9 +14,9 @@ export const getPictures = async (req, res) => {
 }
 
 export const createPicture = async (req, res) => {
-    const keyWord = "blue+balloon"
+    const keyWord = "yellow-balloon"
     const requestOne = await axios.get(`https://pixabay.com/api/?key=31235838-bd5758a4f626905b90d3a2998&q=${keyWord}&image_type=photo`);
-    console.log(requestOne.data.hits)
+    console.log(requestOne.data.hits[1])
     const image = requestOne.data.hits[1].userImageURL
     const size = requestOne.data.hits[1].imageSize
     const foundAt = new Date()
@@ -33,4 +33,27 @@ export const createPicture = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message })
     }
+}
+
+export const updatePicture = async(req, res) => {
+    console.log(req.params)
+    const {id} = req.params
+    const picture = req.body;
+    
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No pic with that id');
+
+    const updatedPicture = await accountModel.findByIdAndUpdate(id, picture, {new: true});
+    
+    res.json(updatedPicture);
+}
+
+export const deletePicture = async(req, res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No acc with that id');
+
+    await accountModel.findByIdAndRemove(id);
+
+
+    res.json({ message: 'Picture deleted successfully' })
 }
--- server/routes/pictures.js
+++ server/routes/pictures.js
@@ -1,10 +1,12 @@
 import express from 'express'
 
-import { getPictures, createPicture } from '../controllers/pictures.js'
+import { getPictures, createPicture, updatePicture, deletePicture } from '../controllers/pictures.js'
 
 const router = express.Router();
 
 router.get('/', getPictures);
-router.post('/', createPicture)
+router.post('/', createPicture);
+router.patch('/:id', updatePicture);
+router.delete('/:id', deletePicture);
 
 export default router;
